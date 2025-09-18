1fab79c4803b97246571838b69cd7a77616e7c00
Mouctar66 <amd62436@gmail.com>
2025-09-18 19:42:07 +0200

    Add Docker configuration for Render6

--- public/test.php
+++ public/test.php
@@ -1,6 +1,7 @@
 <?php
-// public/test.php
 header('Content-Type: text/plain');
-echo "=== TEST SERVEUR ===\n";
-echo "PHP fonctionne correctement!\n";
-echo "Time: " . date('Y-m-d H:i:s');
+echo "=== RENDER TEST ===\n";
+echo "Server: " . $_SERVER['SERVER_SOFTWARE'] . "\n";
+echo "PHP: " . PHP_VERSION . "\n";
+echo "Time: " . date('Y-m-d H:i:s') . "\n";
+echo "URL: " . $_SERVER['REQUEST_URI'] . "\n";
