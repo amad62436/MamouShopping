64c5b0b51836b5c3573575169ad88eba474cb74f
Mouctar66 <amd62436@gmail.com>
2025-09-18 20:04:13 +0200

    Add Docker configuration for Render7

--- public/test.php
+++ public/test.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: text/plain');
-echo "=== RENDER TEST ===\n";
+echo "=== TEST PHP ===\n";
+echo "PHP Version: " . PHP_VERSION . "\n";
 echo "Server: " . $_SERVER['SERVER_SOFTWARE'] . "\n";
-echo "PHP: " . PHP_VERSION . "\n";
-echo "Time: " . date('Y-m-d H:i:s') . "\n";
-echo "URL: " . $_SERVER['REQUEST_URI'] . "\n";
+echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "\n";
+echo "Working Directory: " . getcwd() . "\n";
